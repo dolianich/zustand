e8eaf791539a1d902a10911ef30de2026df0008f
dolianich <69437697+dolianich@users.noreply.github.com>
2024-04-25 13:03:55 +0300

    done

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,9 +1,17 @@
+/* eslint-disable react/prop-types */
 import "./Column.css";
 import Task from "./Task";
+import { useStore } from "zustand";
 
-export default function Column({state}){
-    return <div className="column">
+export default function Column({ state }){
+
+    const tasks = useStore((store) => store.tasks.filter((task) => task.state ===state));
+
+    return (
+    <div className="column">
+
         <p>{state}</p>
-        <Task title='To Do'/>
-    </div>
+        {tasks.map((task) => (<Task title={task.title} key={task.title}/>))}
+        
+    </div>);
 }
